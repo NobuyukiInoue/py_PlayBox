91b49a57316bdb669e716b3a977d8d9d480653df
nobuyuki inoue <spring555@gmail.com>
2018-08-23 21:59:31 +0900

    modified py_PlayBox.py

--- py_PlayBox.py
+++ py_PlayBox.py
@@ -1,15 +1,15 @@
 #!python.exe
 # coding: cp932
 
+import os
 import sys
 from time import sleep
 import ctypes
 from ctypes import *
 
-##-----------------------------------------------------------------##
-## MIDI関連関数コール処理用クラス
-##-----------------------------------------------------------------##
+
 class myMIDI:
+  """MIDI関連関数コール処理用クラス"""
   def __init__(self, initData):
     if (sys.maxsize == 2 ** 63 - 1):
       self.initData = c_int64(initData)
@@ -22,7 +22,7 @@ class myMIDI:
     
   def Init(self):
     ctypes.windll.Winmm.midiOutOpen(byref(self.h), self.MIDI_MAPPER, 0, 0, 0)
-    ctypes.windll.winmm.midiOutShortMsg(self.h, initData)
+    ctypes.windll.winmm.midiOutShortMsg(self.h, self.initData)
 
   def Out(self, outData, length):
     ctypes.windll.winmm.midiOutShortMsg(self.h, outData)
@@ -35,27 +35,25 @@ class myMIDI:
   def Close(self):
     ctypes.windll.winmm.midiOutReset(self.h)
 
-##-----------------------------------------------------------------##
-## 音階定義を格納するクラス
-##-----------------------------------------------------------------##
+
 class ScaleDefs:
+  """音階定義を格納するクラス"""
   def __init__(self, scale, note):
     self.scale = scale
     self.note = note
 
-##-----------------------------------------------------------------##
-## 楽譜データを格納するクラス
-##-----------------------------------------------------------------##
+
 class PlayData:
+  """楽譜データを格納するクラス"""
   def __init__(self, scale, note, length):
     self.scale = scale
     self.note = note
     self.length = length
 
-##-----------------------------------------------------------------##
-## 音階定義ファイルを読み込む
-##-----------------------------------------------------------------##
+
 def loadDefFile(filename):
+  """音階定義ファイルを読み込む"""
+
   # ファイルをオープンする
   defFile = open(filename, "r")
 
@@ -83,10 +81,10 @@ def loadDefFile(filename):
 
   return defs
 
-##-----------------------------------------------------------------##
-## 音階定義ファイルを読み込む
-##-----------------------------------------------------------------##
+
 def loadPlayFile(filename):
+  """音階定義ファイルを読み込む"""
+
   # ファイルをオープンする
   playFile = open(filename, "r")
 
@@ -114,77 +112,82 @@ def loadPlayFile(filename):
 
   return pData
 
-##--------------------------------------------------------##
-## 音階文字列を検索し、ノートナンバーをセットする
-##--------------------------------------------------------##
+
 def replaceScalt_to_Freq(defs, pData):
-  for i in range(len(pData)):
-    scale = pData[i].scale.split(",")
+  """音階文字列を検索し、ノートナンバーをセットする"""
+  for currentData in pData:
+    scale = currentData.scale.split(",")
     for temp in scale:
-      for j in range(len(defs)):
-        if temp == defs[j].scale:
-          if pData[i].note == "":
-            pData[i].note = defs[j].note
+      for currentLen in defs:
+        if temp == currentLen.scale:
+          if currentData.note == "":
+            currentData.note = currentLen.note
           else:
-            pData[i].note += "," + defs[j].note
+            currentData.note += "," + currentLen.note
           break
 
 
-##-----------------------------------------------------------------##
-## Main
-##-----------------------------------------------------------------##
-argv = sys.argv
-argc = len(argv)
+def main():
+  argv = sys.argv
+  argc = len(argv)
 
-if (argc < 2):
-	#引数の個数チェック
-	print('Usage: python %s musicDataFile <timbre>' %argv[0] )
-	quit()
+  if (argc < 2):
+    #引数の個数チェック
+    print('Usage: python %s musicDataFile <timbre>' %argv[0] )
+    quit()
 
-if (argc >= 3):
-    timbre = int(argv[2])
-else:
-    timbre = 1
+  note_number_file = "note-number.dat"
+  if not os.path.exists(note_number_file):
+    print('%s not found.' %note_number_file)
+    quit()
 
-# 音階定義ファイルの読み込み
-defs = loadDefFile("note-number.dat")
-
-# 楽譜ファイルの読み込み
-pData = loadPlayFile(argv[1])
-
-# ノート番号のセット
-replaceScalt_to_Freq(defs, pData)
-
-initData = timbre*256 + 0xc0
-pm = myMIDI(initData)
-pm.Init()
-
-print("")
-print("Load Done. Play Start!!")
-
-for i in range(len(pData)):
-  if pData[i].note != "":
-    print('[%d] = %s( %s ), %s [ms]' %(i, pData[i].scale, pData[i].note, pData[i].length))
-
-    cnote = pData[i].note.split(",")
-
-    for data in cnote:
-      # 鍵盤を押す
-      play_on = "0x7f" + data + "90"
-      pm.OutOnly(int(play_on, 16))
-
-    sleep(int(pData[i].length) / 1000.0)
-
-    for data in cnote:
-      # 鍵盤を離す
-      play_off = "0x7f" + data + "80"
-      pm.OutOnly(int(play_off, 16))
-
+  if (argc >= 3):
+      timbre = int(argv[2])
   else:
-    print('[%d] = rest, %s [ms]' %(i, pData[i].length))
-    
-    # 休符
-    sleep(int(pData[i].length) / 1000.0)
+      timbre = 1
 
-pm.Close()
-print()
+  # 音階定義ファイルの読み込み
+  defs = loadDefFile(note_number_file)
+
+  # 楽譜ファイルの読み込み
+  pData = loadPlayFile(argv[1])
+
+  # ノート番号のセット
+  replaceScalt_to_Freq(defs, pData)
+
+  initData = timbre*256 + 0xc0
+  pm = myMIDI(initData)
+  pm.Init()
+
+  print("\nLoad Done. Play Start!!")
+
+  i = 0
+  for currentpData in pData:
+    if currentpData.note != "":
+      print('[%d] = %s( %s ), %s [ms]' %(i, currentpData.scale, currentpData.note, currentpData.length))
+      cnote = currentpData.note.split(",")
+
+      for data in cnote:
+        # 鍵盤を押す
+        play_on = "0x7f" + data + "90"
+        pm.OutOnly(int(play_on, 16))
+
+      sleep(int(currentpData.length) / 1000.0)
+
+      for data in cnote:
+        # 鍵盤を離す
+        play_off = "0x7f" + data + "80"
+        pm.OutOnly(int(play_off, 16))
+
+    else:
+      print('[%d] = rest, %s [ms]' %(i, currentpData.length))
+      
+      # 休符
+      sleep(int(currentpData.length) / 1000.0)
+      i += 1
+
+  pm.Close()
+  print()
+
+if __name__ == "__main__":
+  main()
